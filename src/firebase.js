8d0a658f00652604e2100e3ce63960b752f34d5b
jabberwock404 <42008427+jabberwock404@users.noreply.github.com>
2022-06-04 16:35:00 -0400

    clean up

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDbgAZwogIqII3da6WW5RRdHF8e7Yjre6M",
+  
+    authDomain: "shuttler-54482.firebaseapp.com",
+  
+    projectId: "shuttler-54482",
+  
+    storageBucket: "shuttler-54482.appspot.com",
+  
+    messagingSenderId: "1066984137330",
+  
+    appId: "1:1066984137330:web:5463d27d7e9a799a0298eb",
+  
+    measurementId: "G-3VK10FB3SM"
+  
+  };
