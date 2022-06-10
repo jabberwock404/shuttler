3190f935f113ccf76e382b61f6c77365b153cb28
jabberwock404 <42008427+jabberwock404@users.noreply.github.com>
2022-06-09 20:42:17 -0400

    added sidebar component
    modified css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import './App.css';
+import Sidebar from "./Sidebar"
 
 function App() {
-  return <div className="App">
-      <h1>lets build shutter</h1>
+  return (
+    <div className="app">
+      <div className="app_body">
+        {/* Application Sidebar */}
+        <Sidebar />
+        {/* Chat Section */}
+      </div>
     </div>
+  );
+  
 }
 
 export default App;
